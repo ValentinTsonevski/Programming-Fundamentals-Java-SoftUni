d3d72c9158036e44ddb2d12f5671eaa402f62bd2
Valentin Tsonevski <tsonevski99@gmail.com>
2022-06-30 16:19:04 +0100

    Add files via upload

---
+++ ObjectAndClasses/src/SumBigNumbers.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+
+        BigInteger sum = num1.add(num2);
+
+        System.out.println(sum);
+
+    }
+}
